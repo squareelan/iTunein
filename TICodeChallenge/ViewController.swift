32a10e7437fa580a49083b985878387dc58e1998
Yongjun Yoo <yyj8909@gmail.com>
2016-07-29 22:34:06 -0500

    Initial Commit

---
+++ TICodeChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TICodeChallenge
+//
+//  Created by Ian on 7/29/16.
+//  Copyright © 2016 Yongjun Yoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
